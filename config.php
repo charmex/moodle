36d888ebd7f56f0b846a99515f3c2dded78997af
Carlos Bonifacio <psistemas@indelpro.com>
2013-12-13 14:58:26 -0600

    Initial commit of the server 07 version

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodnew';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbsocket' => 0,
+);
+
+$CFG->wwwroot   = 'http://localhost:8080/moodnew';
+$CFG->dataroot  = 'C:\\xampp\\moodnewdata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+$CFG->defaultblocks_override = 'completionstatus,navigation,settings:';
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
